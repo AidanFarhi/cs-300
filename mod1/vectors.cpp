24ac542470947b0f821d8c1596f29e1eacc6acc1
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-10-29 06:58:37 -0400

    added more to vectors code

--- mod1/vectors.cpp
+++ mod1/vectors.cpp
@@ -11,5 +11,21 @@ int main() {
     itemCounts.at(2) = 117;
 
     cout << itemCounts.at(1) << endl;
+
+    vector<int> oldestPeople(5);
+    int nthPerson;
+
+    oldestPeople.at(0) = 122;
+    oldestPeople.at(1) = 119;
+    oldestPeople.at(2) = 117;
+    oldestPeople.at(3) = 117;
+    oldestPeople.at(4) = 116;
+
+    cout << "enter N (1..5): ";
+    cin >> nthPerson;
+    if ((nthPerson >= 1) && (nthPerson <= 5)) {
+        cout << "The " << nthPerson << "th oldest person lived";
+        cout << oldestPeople.at(nthPerson - 1) << " years.";
+    }
 }
 
