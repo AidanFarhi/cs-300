eebe38f231d722e914a63681de27df014f7d3cc3
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-11-03 17:31:50 -0500

    added some more textbook example code

---
+++ mod1/arrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int userWeights[3];
+    int userAge;
+
+    userAge = 55;
+
+    userWeights[0] = 199;
+    userWeights[1] = 174;
+    userWeights[2] = 156;
+    userWeights[3] = 201;
+
+    cout << "end" << endl;
+}
